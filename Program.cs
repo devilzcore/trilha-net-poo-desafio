399159890c13dec5a0725c83854fed1670ace07d
devilzcore <aldosilva1@live.com>
2022-11-11 16:34:02 -0300

    Update class Nokia

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "1234", modelo: "1", imei: "34124512", memoria: 12312);
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+
+Console.WriteLine("\n");
